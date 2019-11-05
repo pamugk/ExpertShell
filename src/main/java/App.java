a141d8cba4ba20730a5b04274bbe8d14d76ea5b7
Alexander <pamugk@gmail.com>
2019-11-05 16:28:11 +0500

    Maven transition
    Moved the project on to maven.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,13 +1,6 @@
 import expertshellgui.ExpertShellController;
-import expertsystem.ExpertSystem;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import transfer.BinKnowledgeBaseExporter;
-import transfer.BinKnowledgeBaseImporter;
 
 import java.util.ResourceBundle;
 
